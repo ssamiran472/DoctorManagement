1c5b5d38efce0517818ff2f72e0c26ce0bb11413
Samiran Sarkar <samiransarkar513@gmail.com>
2021-03-09 01:23:29 +0530

    my changes

--- opd/admin.py
+++ opd/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Services
+from .models import Services, BillHeader, BillDetails
 
-admin.site.register(Services)
+admin.site.register((Services, BillHeader, BillDetails))
--- opd/models.py
+++ opd/models.py
@@ -31,9 +31,12 @@ class BillHeader(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     created_by = models.CharField(default='', max_length=100, blank=True)
+    total_bill = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    paid = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-    def __str__(self):
-        return '{0}({1})'.format(self.patient.full_name, self.get_date)
+    # def __str__(self):
+    #     return '{0}({1})'.format(self.patient.full_name, self.get_date)
     
     def get_date(self):
         return self.create_at.strftime("%m/%d/%Y")
@@ -43,6 +46,8 @@ class BillHeader(models.Model):
 class BillDetails(models.Model):
     amount = models.PositiveIntegerField(default=0)
     service = models.ForeignKey(Services, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100, default='')
+    bill_header = models.ForeignKey(BillHeader, on_delete=models.CASCADE, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
